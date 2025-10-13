1cb6ce132b9986054c9162eec0f21fb06a6ac437
Vincent Schall <v.schall@unibas.ch>
2025-10-13 15:36:35 +0200

    Add some more logic to main contract

--- contract/contracts/InheritanceProtocol.sol
+++ contract/contracts/InheritanceProtocol.sol
@@ -11,15 +11,31 @@ contract InheritanceProtocol is Ownable, ReentrancyGuard{
 
 	enum state { ACTIVE, WARNING, VERIFICATION, DISTRIBUTION }
 
-	constructor(address _usdcAddress) Ownable(msg.sender) {
-		
-		require(_usdcAddress != address(0), "USDC address zero");
-		usdc = IERC20(_usdcAddress);
-
+	struct Beneficiary {
+		address payoutAddress;
+		uint amount; // percentage value (0-100)
 	}
 
+	Beneficiary[10] beneficiaries;
 
-	
+	constructor(address _usdcAddress) Ownable(msg.sender) {
+		require(_usdcAddress != address(0), "USDC address zero");
+		usdc = IERC20(_usdcAddress);
+	}
+
+	function getBeneficiaries() public view returns (Beneficiary[10] memory) {
+		return beneficiaries;
+	}
+
+	function isPayoutFullyDetermined() public view returns (bool) {
+		uint256 sum;
+		for (uint i=0; i<10; i++) {
+			if (beneficiaries[i].payoutAddress != address(0)) {
+				sum += beneficiaries[i].amount;
+			}
+		}
+		return sum == 100;
+	}
 
 
 }
