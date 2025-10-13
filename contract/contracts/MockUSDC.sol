311a1c330944c6a77fd2b31c362f0ce728fbcfe5
Vincent Schall <v.schall@unibas.ch>
2025-10-13 15:22:01 +0200

    Add MockUSDC contract for future testing

---
+++ contract/contracts/MockUSDC.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSDC is ERC20 {
+
+	constructor() ERC20("Mock USDC", "mUSDC") {
+		_mint(msg.sender, 1_000_000 * 10 ** decimals());
+	}
+
+	function mint(address to, uint256 amount) external {
+		_mint(to, amount);
+	}
+
+}
+
