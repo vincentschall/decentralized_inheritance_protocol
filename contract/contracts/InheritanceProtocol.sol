e61af1f42679c633eeeefd42c7813207dd192be8
Vincent Schall <v.schall@unibas.ch>
2025-11-10 17:22:43 +0100

    moved mock contracts to new directory

--- contract/contracts/InheritanceProtocol.sol
+++ contract/contracts/InheritanceProtocol.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {IDeathOracle} from "./IDeathOracle.sol";
+import {IDeathOracle} from "./mocks/IDeathOracle.sol";
 
 contract InheritanceProtocol is Ownable, ReentrancyGuard {
 
